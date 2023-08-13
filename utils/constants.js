fb0440ca1ff5f12c9543f7db0e646a9a762bd52d
lesha <oookosmos.m@yandex.ru>
2023-08-13 05:49:11 +0300

    новый бэк

---
+++ utils/constants.js
@@ -0,0 +1,15 @@
+module.exports.DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+module.exports.ALLOWED_DOMAIN_NAME = [
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'http://localhost:3001',
+  'https://localhost:3001',
+  'http://130.193.55.78:3000',
+  'https://130.193.55.78:3000',
+  'http://130.193.55.78:3001',
+  'https://130.193.55.78:3001',
+  'https://api.movie.nomoredomains.work',
+  'http://api.movie.nomoredomains.work',
+  'https://movie-explorer-diplom.nomoredomains.work',
+  'http://movie-explorer-diplom.nomoredomains.work',
+];
